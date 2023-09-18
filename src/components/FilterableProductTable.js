7862231a01376f0f444108dd2fc049453855050c
najwalahly <najwa.lahly@gmail.com>
2023-09-18 15:34:31 +0200

    connect components / add states

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -1,7 +1,11 @@
 import SearchBar from "./SearchBar"
 import ProductTable from "./ProductTable"
+import { useState } from "react";
 
 export default function FilterableProductTable() {
+    const [filterText, setFilterText] = useState('');
+    const [inStockOnly, setInStockOnly] = useState(false);
+
     const PRODUCTS = [
         { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
         { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
@@ -9,12 +13,16 @@ export default function FilterableProductTable() {
         { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
         { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
         { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
-      ];
+    ];
+
 
     return (
         <div>
-            <SearchBar />
-            <ProductTable products={PRODUCTS}/>
+            <SearchBar filterText={filterText}
+                inStockOnly={inStockOnly}
+                onFilterTextChange={setFilterText}
+                onInStockOnlyChange={setInStockOnly} />
+            <ProductTable products={PRODUCTS} filterText={filterText} inStockOnly={inStockOnly} />
         </div>
     )
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,9 @@
-export default function SearchBar() {
+export default function SearchBar({ filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange}) {
     return (
         <form>
-            <input type="text" placeholder="Search..." />
+            <input type="text" placeholder="Search..." onChange={(e) => onFilterTextChange(e.target.value)}/>
             <label>
-                <input type="checkbox" />
+                <input type="checkbox" onChange={(e) => onInStockOnlyChange(e.target.checked)}/>
                 Only show products in stock
             </label>
         </form>
