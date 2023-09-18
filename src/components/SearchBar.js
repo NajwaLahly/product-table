feae76b8779abc6a6d58798325df66a1b9e3446f
najwalahly <najwa.lahly@gmail.com>
2023-09-18 14:36:13 +0200

    add static components

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+export default function SearchBar() {
+    return (
+        <form>
+            <input type="text" placeholder="Search..." />
+            <label>
+                <input type="checkbox" />
+                Only show products in stock
+            </label>
+        </form>
+    )
+}
